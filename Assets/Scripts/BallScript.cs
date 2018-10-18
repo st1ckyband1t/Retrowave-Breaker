5d60a62acead073dd8b4418d3c5e598f7ef290ec
Sunny David <sunny.david.04@gmail.com>
2018-10-18 10:43:40 +0530

    Added Script for Ball

--- Assets/Scripts/BallScript.cs
+++ Assets/Scripts/BallScript.cs
@@ -4,16 +4,22 @@ using UnityEngine;
 
 public class BallScript : MonoBehaviour {
 
+    // Config parameters
     [SerializeField] Paddle paddle1;
+
+    
+    //state
+    Vector2 paddleToBallVec;
 
 
 	// Use this for initialization
 	void Start () {
-		
+        paddleToBallVec = transform.position - paddle1.transform.position;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        Vector2 paddlePos = new Vector2(paddle1.transform.position.x, paddle1.transform.position.y);
+        transform.position = paddlePos + paddleToBallVec;
 	}
 }
