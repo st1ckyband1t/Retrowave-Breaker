ea44227424f776491f0a772cdadeba7f8f1d7515
Sunny David <sunny.david.04@gmail.com>
2018-10-18 08:25:11 +0530

    First Commit

---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour {
+
+    //config parameters
+
+    [SerializeField] float screenWidthInWorldUnits = 16f;
+    [SerializeField] float minX = 1.54f;
+    [SerializeField] float maxX = 14.45f;
+    
+    // Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        var mousePosInUnits = Input.mousePosition.x / Screen.width * screenWidthInWorldUnits;
+        Vector2 paddlePos = new Vector2(transform.position.x, transform.position.y);
+        paddlePos.x = Mathf.Clamp(mousePosInUnits, minX, maxX);
+        transform.position = paddlePos;
+	}
+}
---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour {
+
+    public void LoadMainScene()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+	
+	
+}
