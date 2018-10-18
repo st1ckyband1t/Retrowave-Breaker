3a5793c22ece03f3bdf9c9d5c86f9107b8f2e578
Sunny David <sunny.david.04@gmail.com>
2018-10-18 16:56:10 +0530

    Added launching functionality to BallScript

--- Assets/Scripts/BallScript.cs
+++ Assets/Scripts/BallScript.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,20 +7,41 @@ public class BallScript : MonoBehaviour {
 
     // Config parameters
     [SerializeField] Paddle paddle1;
-
-    
+    [SerializeField] float xPushVel = 2f;
+    [SerializeField] float yPushVel = 15f;
     //state
     Vector2 paddleToBallVec;
+    bool hasStarted = false;
 
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start ()
+    {
         paddleToBallVec = transform.position - paddle1.transform.position;
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+    {
+        if (!hasStarted)
+        {
+            LockBallToPaddle();
+            LaunchOnMouseClick();
+        }    
+    }
+
+    private void LaunchOnMouseClick()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            hasStarted = true;
+            GetComponent<Rigidbody2D>().velocity = new Vector2(xPushVel, yPushVel);
+        }
+    }
+
+    private void LockBallToPaddle()
+    {
         Vector2 paddlePos = new Vector2(paddle1.transform.position.x, paddle1.transform.position.y);
-        transform.position = paddlePos + paddleToBallVec;
-	}
+        transform.position = paddlePos + paddleToBallVec; 
+    }
 }
