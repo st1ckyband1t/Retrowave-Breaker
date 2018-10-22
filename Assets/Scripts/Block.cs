1831c4f4963c7958c00b5fa031945d98360d3a71
Sunny David <sunny.david.04@gmail.com>
2018-10-22 07:16:45 +0530

    Added level win functionality

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -6,9 +6,25 @@ public class Block : MonoBehaviour {
 
     [SerializeField] AudioClip breaksound;
     [SerializeField] float volume = 0.5f;
+
+    //cached reference
+    Level level;
+
+    private void Start()
+    {
+        level = FindObjectOfType<Level>();
+        level.CountBreakableBlocks();
+    }
+
 	private void OnCollisionEnter2D(Collision2D collision)
+    {
+        DestroyBlock();
+    }
+
+    private void DestroyBlock()
     {
         AudioSource.PlayClipAtPoint(breaksound, Camera.main.transform.position, volume);
         Destroy(gameObject);
+        level.BlockDestroyed();
     }
 }
---
+++ Assets/Scripts/Level.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level : MonoBehaviour {
+
+    //parameters
+    [SerializeField] int breakableBlocks; //can be used for debugging purposes
+
+    //cache reference
+    SceneLoader sceneLoader;
+
+    private void Start()
+    {
+        sceneLoader = FindObjectOfType<SceneLoader>();
+    }
+
+    public void CountBreakableBlocks()
+    {
+        ++breakableBlocks;
+    }
+
+    public void BlockDestroyed()
+    {
+        --breakableBlocks;
+        if(breakableBlocks <= 0)
+        {
+            sceneLoader.LoadMainScene();
+        }
+    }
+
+
+}
