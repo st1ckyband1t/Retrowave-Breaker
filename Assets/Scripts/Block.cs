5828966eb6bb165c28ef4caf742f6aed425df41b
Sunny David <sunny.david.04@gmail.com>
2018-10-24 17:47:51 +0530

    Added particle effects

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -6,6 +6,7 @@ public class Block : MonoBehaviour {
 
     [SerializeField] AudioClip breaksound;
     [SerializeField] float volume = 0.5f;
+    [SerializeField] GameObject particleEffectVFX;
 
     //cached reference
     Level level;
@@ -25,9 +26,21 @@ public class Block : MonoBehaviour {
 
     private void DestroyBlock()
     {
-        AudioSource.PlayClipAtPoint(breaksound, Camera.main.transform.position, volume);
+        PlayImpactAudio();
         Destroy(gameObject);
+        ParticleVFX();
         level.BlockDestroyed();
         score.AddScore();
+    }
+
+    private void PlayImpactAudio()
+    {
+        AudioSource.PlayClipAtPoint(breaksound, Camera.main.transform.position, volume);
+    }
+
+    private void ParticleVFX()
+    {
+        GameObject impact = Instantiate(particleEffectVFX, transform.position, transform.rotation);
+        Destroy(impact, 1f);
     }
 }
